420518e6f24d90d2a9d82402174d5714f6027dfa
Gston45 <gkowou5@gmail.com>
2025-07-25 01:12:43 +0000

    Upload des scripts Python (30DaysofPython)

---
+++ Day1/helloword2.py
@@ -0,0 +1,16 @@
+'''
+h = 10
+print(type(h))
+
+i = 9.8
+print(type(i))
+
+j = 3.14
+print(type(j))
+
+k = 4 -4J
+print(type(k))
+
+l = ['Asabeneh', 'Python', 'Finlande']
+print(type(l))
+'''
---
+++ Day1/helloword3.py
@@ -0,0 +1,11 @@
+#Distance euclidienne
+#formule = √((x2-x1)+(y2-y1))
+alpha = (a-b)+(c-d)
+#Distance = √(alpha)
+
+a = 2
+b = 10
+c = 3
+d = 8
+
+print("alpha")
