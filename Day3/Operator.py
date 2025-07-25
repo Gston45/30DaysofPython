52590537dcca6f7f4d12cff38746476d4511be58
Gston45 <gkowou5@gmail.com>
2025-07-25 21:36:36 +0000

    Upload des scripts Python (30DaysofPython)

---
+++ Day3/Operator.py
@@ -0,0 +1,101 @@
+'''
+#1 Declare age
+age = 20
+age = int(age)
+
+#2 Declare Height
+
+height = 1.81
+height = float(height)
+
+#3 complex number
+
+num = 4 + 9j
+
+#4 Script
+
+b = input("Enter base :")
+
+h = input("Enter height :")
+
+b = int(b)
+h = int(h)
+
+area = 0.5 * b * h
+
+print("The area of the triangle is :", area)
+
+#5 Script 
+
+a = input("Enter side a :")
+
+b = input("Enter side b :")
+
+c = input("Enter side c :")
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+perimeter = a + b + c
+
+print("The perimeter of the triangle is :", perimeter)
+
+
+#6 length and width of a rectangle using prompt
+
+lenght = input("Enter lenght :")
+
+width = input("Enter width :")
+
+lenght = int(lenght)    
+width = int(width)      
+
+perimeter = 2 * (lenght + width)
+
+area = lenght * width
+
+print("erea is : ", area)
+
+print("perimeter is :", perimeter)
+
+#7 radius of a circle using prompt
+
+r = input("Enter radius :")
+
+r = int(r)
+pi = 3.14
+
+area = pi * r * r
+
+c = 2 * pi * r
+
+print("The area is :", area, ",", "Te circumference is :", c)
+'''
+
+
+#8 The slope 
+
+a = input("Enter x1 :")   , b = input("y1 :")
+c = input("x2:")    , d = input("y2 :")
+
+a = int(a)  , b = int(b)
+c = int(c)  , d = int(d)
+
+b = 2 * a - 2
+d = 2 * c - 2
+
+slope = d - b / c - a
+
+print("slope =", slope)
+
+'''
+#9 The slope
+
+x1 = 2 , y1 = 2
+x2 = 6 , y2 = 10
+
+m = y2-y1/x2-x1
+
+print(m)
+'''
