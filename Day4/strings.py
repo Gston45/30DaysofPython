c3c3919f55391d551b598d0090b721d164208149
Gston45 <gkowou5@gmail.com>
2025-07-26 23:37:12 +0000

    Upload des scripts Python (30DaysofPython)

--- Day4/strings.py
+++ Day4/strings.py
@@ -1,5 +1,116 @@
-first_name = 'ama'
-last_name =  'kodjo'
-language  = 'python'
-formated_string = 'i am  %s %s. I teach %s' %(first_name, last_name, language)
-print(formated_string)
+#1
+a = "Thirty"
+b = "Days"
+c = "Of"
+d = "Python"
+Thirty_Days_Of_Python = a + ' '  + b + ' ' + c + ' ' + d
+print(Thirty_Days_Of_Python)
+
+#2
+a = "Coding"
+b = "For"
+c = "All"
+
+Coding_For_All = a + ' ' + b + ' ' + c
+print(Coding_For_All)
+
+#3
+company = "Coding For All"
+
+#4
+print(company)
+
+#5
+print(len(company))
+
+#6
+print(company.upper())
+
+#7
+print(company.lower())
+
+#8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+#9
+slice = company[0]
+print(slice)
+
+#10
+Coding = "Coding"
+print(company.index(Coding))
+
+#11
+company = 'Python For All'
+
+#12
+print(company.replace('All', 'Everyone'))
+
+#13
+print(company.split())
+
+#14
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon")
+
+#15
+print(company[0])
+
+#16
+print(company[-1])
+
+#17
+print(company[10])
+
+#18
+PFE = "Python For Everyone"
+
+#19
+CFA = "Coding For All"
+
+#20
+Coding_For_All = "Coding For All"
+C = 'C'
+
+print(Coding_For_All.index(C))
+
+#21
+F = 'F'
+print(Coding_For_All.index(F))
+
+#22
+l = 'l'
+print(Coding_For_All.rfind(l))
+
+#23
+sentence = "You cannot end a sentence with because because because is a conjunction"
+because = 'because'
+
+print(sentence.index(because))
+
+#24
+print(sentence.rindex(because))
+
+'''#25
+slice = sentence.find('because because because')
+
+print(slice)
+'''
+#26
+print(sentence.find(because))
+
+#27 same at 25
+
+#28
+print(CFA.find(Coding)is[0])
+
+#29
+coding = 'coding'
+print(CFA.find(coding)is[0])
+
+#30
+txt = ' Coding For All '
+print(txt.strip())
+
+#31
