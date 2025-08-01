4fb19ba504450b8201e6803fe566417bc022d5d1
Gston45 <gkowou5@gmail.com>
2025-08-01 23:29:20 +0000

    Upload des scripts Python (30DaysofPython)

---
+++ Day10/Loops.py
@@ -0,0 +1,56 @@
+#Exercices: Level 1
+#1
+'''
+i = 0
+while i < 11:
+    print(i)
+    i = i + 1
+
+#2
+i = 10
+while i > 0:
+    print(i)
+    i = i - 1
+  
+#3
+for i in range(1, 8):
+    print("#" * i)
+
+#4
+
+for i in range(8):
+    for j in range(8):
+        print('#', end=" ")
+    print()
+
+#5
+for i in range(11):
+    print(f"{i} x {i} = {i * i}")
+
+#6
+list = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for i in list:
+    print(i)
+    
+
+#7
+numbers = 0
+for i in numbers  0:
+    print(i)
+    
+
+a = 0
+while a <= 10:
+    print(a)
+    a = a + 1
+
+
+
+a = range(11)
+for i in a:
+    print(i)
+
+    '''
+a = range(10, -1, -1)
+for i in a:
+    print(i)
