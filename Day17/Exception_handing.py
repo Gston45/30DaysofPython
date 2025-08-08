bcbd67486ee57097b2856de4fe53043f6fee17ad
Gston45 <gkowou5@gmail.com>
2025-08-08 21:49:39 +0000

    Upload des scripts Python (30DaysofPython)

---
+++ Day17/Exception_handing.py
@@ -0,0 +1,11 @@
+#1 (unpack the first five country)
+name = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland', 'Estonia', 'Russia']
+*first_five_country, es, ru= name
+print(first_five_country)
+
+#2 (store the unpacked country to a variable)
+
+*nordic_country, es, ru = name
+print(nordic_country)
+print(es)
+print(ru)
