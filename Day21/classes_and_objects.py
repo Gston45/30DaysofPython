ebe7c1af73733418c8edc889e6b3ef3b6058b397
Gston45 <gkowou5@gmail.com>
2025-08-12 20:15:19 +0000

    Upload des scripts Python (30DaysofPython)

---
+++ Day21/classes_and_objects.py
@@ -0,0 +1,53 @@
+#Exercices: Level 1
+
+class statistics:
+    def __init__(self, data):
+        def count(self):
+            pass
+        def Sum(self):
+            pass
+        def Min(self):
+            pass
+        def Max(self):
+            pass
+        def Range(self):
+            pass
+        def Mean(self):
+            pass
+        def Median(self):
+            pass
+        def Mode(self):
+            pass
+        def Variance(self):
+            pass
+        def Standard_Deviation(self):
+            pass
+        def Frequency_Distribution(self):
+            pass
+
+#Exercices: Level 2
+
+class PersonAccount:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.incomes = []   
+        self.expenses = []  
+
+    def add_income(self, amount, description):
+        pass  
+
+    def add_expense(self, amount, description):
+        pass  
+
+    def total_income(self):
+        pass  
+
+    def total_expense(self):
+        pass  
+
+    def account_balance(self):
+        pass  
+
+    def account_info(self):
+        pass  
